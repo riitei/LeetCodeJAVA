39eabfe7c05b90efd7c2a887eb099d966045ab15
riitei <2590119197@qq.com>
2018-08-15 11:56:45 +0800

    LeetCode Project

---
+++ src/leetcode/SameTree.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ *
+ * @author riitei
+ * https://leetcode.com/problems/same-tree/description/
+ * https://leetcode-cn.com/problems/same-tree/description/
+ * [1,2,3] [1,2,3] true
+ * [1,null,3] [1,2] false
+ * 
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        
+        return false;
+    }
+    
+}
---
+++ src/leetcode/SumOfTwoIntegers.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leetcode;
+
+/**
+ *
+ * @author riitei https://leetcode.com/problems/sum-of-two-integers/description/
+ *
+ * Given a = 1 and b = 2, return 3.
+ *
+ */
+public class SumOfTwoIntegers {
+
+    public static void main(String[] args) {
+        
+    }
+}
